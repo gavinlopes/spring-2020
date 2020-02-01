60de9fd00b83da7069e31c73b3568d7733527ef6
Unknown <gavin.l.lopes@gmail.com>
2020-02-01 00:07:47 -0700

    debug prac

---
+++ Scripting_Prac/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Welcome to number wizard people");
+		Debug.Log("Welcome");
+		Debug.Log("Pick a number");
+		Debug.Log("Highest number is 100");
+		Debug.Log("Lowest number is 1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
