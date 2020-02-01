407367fa0a2a09f69ef6f43cfd9e1229a0677301
Unknown <gavin.l.lopes@gmail.com>
2020-02-01 14:55:12 -0700

    need to update git

--- Scripting_Prac/Assets/Scripts/NumberWizard.cs
+++ Scripting_Prac/Assets/Scripts/NumberWizard.cs
@@ -8,16 +8,33 @@ public class NumberWizard : MonoBehaviour {
 	void Start ()
 	{
 		int max = 1000;
+		int min = 1;
 		
 		Debug.Log("Welcome to number wizard people");
 		Debug.Log("Welcome");
 		Debug.Log("Pick a number");
 		Debug.Log("Highest number is: " + max);
-		Debug.Log("Lowest number is 1");
+		Debug.Log("Lowest number is: " + min);
+		Debug.Log("Tell me if your number is higher or lower than 500");
+		Debug.Log("Push up = Higher, Push down = Lower, Push enter if correct");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Debug.Log("Up Arrow key was pressed.");
+		}
+
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Debug.Log("Down Arrow key was pressed.");
+		}
+
+		else if (Input.GetKeyDown(KeyCode.Return))
+		{
+			Debug.Log("Return key was pressed");
+		}
 	}
 }
