8e67932c9a859c6f2d26cc243a5d16e312cf6433
gavin <gavin.l.lopes@gmail.com>
2020-02-04 16:37:46 -0700

    Movement!
    thanks Brandon

---
+++ marty/game_design_game/Assets/Sprint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Sprint : MonoBehaviour
+{
+    public UnityEvent spaceDown, spaceUp;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            spaceDown.Invoke();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            spaceUp.Invoke();
+        }
+        
+
+
+    }
+}
