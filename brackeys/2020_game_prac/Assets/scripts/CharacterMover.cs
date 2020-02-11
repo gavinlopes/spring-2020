5b1f642c6ef653bf296f297e9f565c9a3b3dcfd7
Unknown <gavin.l.lopes@gmail.com>
2020-02-10 17:51:45 -0700

    healthbar

---
+++ brackeys/2020_game_prac/Assets/scripts/CharacterMover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class CharacterMover : MonoBehaviour
+
+{
+    private CharacterController controller;
+
+    public Vector3 position;
+    public FloatData moveSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.Set(Input.GetAxis("Horizontal") * moveSpeed.value,0, Input.GetAxis("Vertical") * moveSpeed.value);
+        if (position != Vector3.zero)
+        {
+            controller.transform.forward = position;
+        }
+        controller.Move(position * Time.deltaTime);
+
+    }
+}
+
