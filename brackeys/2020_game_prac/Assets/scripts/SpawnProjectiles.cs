9a401b36924a5b34f57d848a46cb40178e092f08
Unknown <gavin.l.lopes@gmail.com>
2020-02-13 00:00:43 -0700

    projectiles

---
+++ brackeys/2020_game_prac/Assets/scripts/SpawnProjectiles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectiles : MonoBehaviour
+{
+    public GameObject firePoint;
+    public List<GameObject> vfx = new List<GameObject>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
