49cf7e7829b857712267d2d76ab034b8772fc365
Unknown <gavin.l.lopes@gmail.com>
2020-02-13 00:19:58 -0700

    projectiles update

--- brackeys/2020_game_prac/Assets/scripts/SpawnProjectiles.cs
+++ brackeys/2020_game_prac/Assets/scripts/SpawnProjectiles.cs
@@ -6,16 +6,23 @@ public class SpawnProjectiles : MonoBehaviour
 {
     public GameObject firePoint;
     public List<GameObject> vfx = new List<GameObject>();
+
+    private GameObject effectToSpawn;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        effectToSpawn = vfx[0];
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
+            SpawnVFX();
+        }
         
     }
 }
