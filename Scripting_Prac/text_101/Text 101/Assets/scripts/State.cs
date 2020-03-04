ff4456a10c3f38886bc1c03a40f5d9157db49af0
Unknown <gavin.l.lopes@gmail.com>
2020-03-03 23:54:14 -0700

    text game update

--- Scripting_Prac/text_101/Text 101/Assets/scripts/State.cs
+++ Scripting_Prac/text_101/Text 101/Assets/scripts/State.cs
@@ -8,5 +8,12 @@ public class State : ScriptableObject
 {
    [TextArea(10,14)][SerializeField] string storyText;
 
+   public string GetStateStory()
+   {
+      return storyText;
+   }
+  
+   
+
 
 }
