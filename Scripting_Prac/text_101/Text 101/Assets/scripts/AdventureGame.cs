32bca2b9ab4dcfd3c1fa81ac0f4c7cc3761b6d66
Unknown <gavin.l.lopes@gmail.com>
2020-03-28 23:43:20 -0600

    text update if states

--- Scripting_Prac/text_101/Text 101/Assets/scripts/AdventureGame.cs
+++ Scripting_Prac/text_101/Text 101/Assets/scripts/AdventureGame.cs
@@ -27,5 +27,22 @@ public class AdventureGame : MonoBehaviour
     private void ManageState()
     {
         var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+
+        textComponent.text = state.GetStateStory();
+
+
     }
 }
