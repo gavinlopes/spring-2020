3bfef90e66497de34c405298fba47da57d0e1bf2
Unknown <gavin.l.lopes@gmail.com>
2020-04-06 03:17:08 -0600

    map update

---
+++ brackeys/2020_game_prac/Assets/scripts/GameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    private bool gameEnded = false;
+    
+    
+
+    private void Update()
+    {
+        if (gameEnded)
+            return;
+
+        if (CharacterStats <- 0 )
+        {
+            EndGame();
+        }
+    }
+
+    public int CharacterStats { get; set; }
+
+    void EndGame()
+    {
+        gameEnded = true;
+        Debug.Log("Game Over!");
+    }
+}
