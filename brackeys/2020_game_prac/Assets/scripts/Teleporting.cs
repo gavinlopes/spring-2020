d52f57ef7e3406dc0e703126f60f2df37349b7e9
Unknown <gavin.l.lopes@gmail.com>
2020-04-20 22:12:50 -0600

    teleport and animation complete

---
+++ brackeys/2020_game_prac/Assets/scripts/Teleporting.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporting : MonoBehaviour
+{
+    public Transform teleportPlayer;
+    public GameObject thePlayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        thePlayer.transform.position = teleportPlayer.transform.position;
+    }
+}
